6febcf1d12d838fbfa0d2e95b524049bae0e4a30
Mike <shinobinz@gmail.com>
2014-04-05 16:04:32 +1300

    alpha_fade_in alpha_fade_out alpha_delta_factor added as part of struct tex_circle

--- jni/gfx/tex_circle.h
+++ jni/gfx/tex_circle.h
@@ -20,14 +20,19 @@ struct tex_circle {
 
 	float alpha;
 	float alpha_max;
-	float alpha_delta_factor;
+//	float alpha_delta_factor;
+	float alpha_fade_in;
+	float alpha_fade_out;
+
 	float scale;
+	float scale_change_rate;
 
 	struct texture_file* tex;
 };
 
 void init_tex_circles();
 void activate_tex_circle(float x, float y, struct vertex_rgb* rgb_p, float* vel);
+void activate_tex_no_ammo(float x, float y, float* vel);
 void kill_all_tex_circles();
 void tex_circle_alpha_size(struct tex_circle* ts);
 void tex_ripple_alpha_size(struct tex_circle* tr);
